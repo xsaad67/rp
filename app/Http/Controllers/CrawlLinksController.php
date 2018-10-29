7b0f4fa63a6926c1635006161386cff9edc789bf
saad <saadsuri67@gmail.com>
2018-10-29 01:17:08 -0700

    epi

--- app/Http/Controllers/CrawlLinksController.php
+++ app/Http/Controllers/CrawlLinksController.php
@@ -9,7 +9,7 @@ use App\RecipeIngridents;
 
 use Goutte;
 
-ini_set('max_execution_time', 180);
+ini_set('max_execution_time', 380);
 
 class CrawlLinksController extends Controller
 {
@@ -30,7 +30,7 @@ class CrawlLinksController extends Controller
 	    		if($isRecipe[1]==="recipes"){
 	    			$links = CrawlLinks::firstOrNew(['link'=>"https://www.epicurious.com".$link]);
 	    			$links->website = "epicurious";
-	    			$links->cuisine = 1;
+	    			$links->cuisine = 'american';
 	    			$links->save();
 
 	    			// session()->push("links", ["link"=> $link, "title" => $node->attr("title"), "website"=>"epicurious",""] );
@@ -46,50 +46,42 @@ class CrawlLinksController extends Controller
 
     public function epicriciousSingle(){
 
-    	// dd(session("links"));
-
     	
-    	$link = "https://www.epicurious.com/recipes/food/views/sourdough-bread-with-marinated-manchego-cheese-and-roasted-peppers-107684";
+        $links = CrawlLinks::where('isActive','0')->limit(50)->get();
+        
 
-    	$recipe= new Recipe();
+        foreach($links as $link){
 
-        $crawler = Goutte::request('GET', $link);
+            $recipe= new Recipe();
+            $crawler = Goutte::request('GET', $link->link);
+            $recipe->description = $crawler->filter("div.dek > p")->count() > 0 ? $crawler->filter("div.dek > p")->text() :  'nope';
+            $recipe->featuredImage = $crawler->filter("img.photo")->count() > 0 ? $crawler->filter("img.photo")->attr("srcset") :  NULL;
+            $recipe->title = $crawler->filter('h1[itemprop="name"]')->text();
+            $recipe->apiLink = $link->link;
+            
+            $serving = $crawler->filter('dd[itemprop="recipeYield"]')->count() > 0 ? $crawler->filter('dd[itemprop="recipeYield"]')->text() : NULL;
+            $recipe->serves =  preg_replace('/[^0-9.]/','',$serving);
+            $recipe->user_id =1;
+            $recipe->cuisine =1;
+            
+            $isSave=$recipe->save();
 
-        $recipe->description = $crawler->filter("div.dek > p")->count() > 0 ? $crawler->filter("div.dek > p")->text() :  'nope';
+            $lastSaveId= $recipe->id;
+            $ingridents = $crawler->filter("ul.ingredients");
+            $ingridents->filter("li")->each(function($in) use ($lastSaveId){
+                $ingridents = new RecipeIngridents();
+                $ingridents->note = $in->text();
+                $ingridents->recipe_id = $lastSaveId;
+                $ingridents->save();
+            });
 
+            if($isSave){
+                $link->isActive =1;
+                $link->save();
+            }
 
-        $recipe->featuredImage = $crawler->filter("img.photo")->count() > 0 ? $crawler->filter("img.photo")->attr("src") :  NULL;
-      	
-    
-    	 $recipe->title = $crawler->filter('h1[itemprop="name"]')->text();
-
-
-    	 $serving = $crawler->filter('dd[itemprop="recipeYield"]')->text();
-    	 $recipe->serves =  preg_replace('/[^0-9.]/','',$serving);
-    	 $recipe->user_id =1;
-    	 $recipe->cuisine =1;
+        }
     	
-    	$recipe->save();
-
-    	$lastSaveId= $recipe->id;
-    	
-    	 $ingridents = $crawler->filter("ul.ingredients");
-
-    	 $ingridents->filter("li")->each(function($in) use ($lastSaveId){
-    	 	$ingridents = new RecipeIngridents();
-    	 	$ingridents->note = $in->text();
-    	 	$ingridents->recipe_id = $lastSaveId;
-    	 	$ingridents->save();
-         });
-
-
-    	// $links = CrawlLinks::where('isActive','1')->get();
-
-    	// foreach($links as $link){
-    	//  $link->isActive =0;
-    	//  $link->save();
-    	// }
-
          
     }
 
