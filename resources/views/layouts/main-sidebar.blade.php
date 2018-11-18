1a752dab3d150e77ecccce15b1034e22ed7c73f0
saad <saadsuri67@gmail.com>
2018-11-18 13:27:55 +0500

    new template

---
+++ resources/views/layouts/main-sidebar.blade.php
@@ -0,0 +1,64 @@
+ <div class="content-sidebar">
+                    <div class="sidebar_inner">
+
+                      
+
+
+                        <div class="widget-item">
+                            <h4 class="w-title">Populer Posts</h4>
+                            <ul class="w-post-list">
+                                <li>
+                                    <a href="#">
+                                        <div class="w-post-image">
+                                            <img src="/img/news-test-images/news-img1.jpg" width="80" height="80">
+                                            <span>1</span>
+                                        </div>
+                                        <div class="w-post-content">
+                                            <span class="w-post-title">Things to Consider When Choosing City Moving Companies</span>
+                                            <span class="w-post-views">356 views</span>
+                                        </div>
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href="#">
+                                        <div class="w-post-image">
+                                            <img src="/img/news-test-images/news-img2.jpg" width="80" height="80">
+                                            <span>2</span>
+                                        </div>
+                                        <div class="w-post-content">
+                                            <span class="w-post-title">Things to Consider When Choosing City Moving Companies</span>
+                                            <span class="w-post-views">356 views</span>
+                                        </div>
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href="#">
+                                        <div class="w-post-image">
+                                            <img src="/img/news-test-images/news-img3.jpg" width="80" height="80">
+                                            <span>3</span>
+                                        </div>
+                                        <div class="w-post-content">
+                                            <span class="w-post-title">Things to Consider When Choosing City Moving Companies</span>
+                                            <span class="w-post-views">356 views</span>
+                                        </div>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div class="seperator"></div>
+
+                        <a href="#" class="widget-ad-box">
+                            <img src="/img/adbox300x250.png" width="300" height="250">
+                        </a>
+
+                        <div class="seperator"></div>
+
+                        <a href="#" class="widget-ad-box">
+                            <img src="/img/adbox300x250.png" width="300" height="250">
+                        </a>
+
+                    </div>
+                </div>
